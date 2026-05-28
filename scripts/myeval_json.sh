8af29852eeacb5a05e35c5e1aae7a96cdcf0aaf5
instincts <2606391015@qq.com>
2026-05-28 19:16:32 +0800

    init

--- scripts/myeval_json.sh
+++ scripts/myeval_json.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=0
 # 必填参数：根据你实际情况修改
 MODEL="repvit_m1_0"
 CKPT_PATH=(
-  "./checkpoints/repvit_m1_0/2026_02_26_21_16_50/checkpoint_best.pth"
+  "./checkpoints/BM/repvit_m1_0_224_0.025_0.001_0.25/checkpoint_best.pth"
   "./checkpoints/LNMCN01/repvit_m1_0_224_0.025_0.001_0.25/checkpoint_best.pth"
   "./checkpoints/FTCPTC/repvit_m1_0_224_0.025_0.001_0.25/checkpoint_best.pth"
 )
