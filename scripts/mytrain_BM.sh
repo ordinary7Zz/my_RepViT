94eaafde8316acd1cc164d9e1d9bbb4b5e94cf3d
instincts <2606391015@qq.com>
2026-05-28 19:02:02 +0800

    init

--- scripts/mytrain_BM.sh
+++ scripts/mytrain_BM.sh
@@ -5,9 +5,9 @@ python train_explicit_paths.py \
   --train-data-path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Superimposed_multitask/dataset_3_cls/train \
   --test-data-path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Superimposed_multitask/dataset_3_cls/test \
   --batch-size 16 \
-  --epochs 40 \
+  --epochs 5 \
   --dist-eval \
-  --output_dir checkpoints \
+  --output_dir checkpoints/BM \
   --finetune pretrain/repvit_m1_0_distill_300e.pth \
   --set_bn_eval \
   --distillation-type none \
